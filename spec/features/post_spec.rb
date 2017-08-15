83fee300f8b35a010138622286ddb86af3c4cc4b
Mark Albright <kalani729@gmail.com>
2017-08-15 15:15:44 -0600

    Implemented edit and update functionality

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-    user = FactoryGirl.create(:user)
-    login_as(user, :scope => :user)
+    @user = FactoryGirl.create(:user)
+    login_as(@user, :scope => :user)
   end
 
   describe 'index' do
@@ -28,10 +28,10 @@ describe 'navigate' do
   end
 
   describe 'creation' do
-    before do     
+    before do
       visit new_post_path
     end
-      
+
     it 'has a new form that can be reached' do
       expect(page.status_code).to eq(200)
     end
@@ -50,6 +50,29 @@ describe 'navigate' do
       click_on "Save"
 
       expect(User.last.posts.last.rationale).to eq("User Association")
+    end
+  end
+
+  describe 'edit' do
+    before do
+      @post = FactoryGirl.create(:post)
+    end
+
+    it 'can be reached by clicking edit on index page' do
+      visit posts_path
+
+      click_link("edit_#{@post.id}")
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be edited' do
+      visit edit_post_path(@post)
+
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Edited content"
+      click_on "Save"
+
+      expect(page).to have_content("Edited content")
     end
   end
 end

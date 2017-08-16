5021617b96d9d0753a7c282f4a3ceb05fa09da23
Mark Albright <kalani729@gmail.com>
2017-08-16 13:56:20 -0600

    Customized admin dashboard user forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,11 @@
 
 puts "1 User created"
 
+AdminUser.create!(first_name: "Admin", last_name: "User", email: "test1@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+
+
+puts "1 Admin User created"
+
 10.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end

d0dd13a4101c5a0bebe39e6f7020b451ba65ff9d
Mark Albright <kalani729@gmail.com>
2017-08-23 15:09:45 -0600

    Blocked non admin users from accessing  index pages for audits

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -13,13 +13,19 @@ describe 'AuditLog Feature' do
       expect(page.status_code).to eq(200)
     end
 
-    it 'render auidt log content' do
+    it 'render audit log content' do
       visit audit_logs_path
       expect(page).to have_content(/SNOW/)
     end
 
-    xit 'cannot be accessed by non admin users' do
+    it 'cannot be accessed by non admin users' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
 
+      visit audit_logs_path
+
+      expect(current_path).to eq(root_path)
     end
   end
 end

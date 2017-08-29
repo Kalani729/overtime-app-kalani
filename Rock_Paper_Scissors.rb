301e1934fc5206e4fb97cb7bcac6c0c272819c9d
Mark Albright <kalani729@gmail.com>
2017-08-29 14:58:22 -0600

    removed unwanted file

--- Rock_Paper_Scissors.rb
+++
@@ -1,9 +0,0 @@
-puts "Choose Rock (r), Paper (p), Scissors (s), Spock (v), Lizard (l)"  
-comp = %w(rock paper scissors spock lizard).sample
-
-player = gets.chomp.downcase
-
-if player == comp
-  puts ""
-  
-  

c8833652f73f8befa3d731211409186d2eed054d
Mark Albright <kalani729@gmail.com>
2017-08-29 15:49:41 -0600

    Integrated callback for rejections

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  resources :audit_logs, except: [:new, :edit, :destroy] do
+  resources :audit_logs, except: [:new, :edit, :destory] do
     member do
       get :confirm
     end
   end
-
+  
   namespace :admin do
     resources :users
-    resources :posts
+		resources :posts
     resources :admin_users
+		resources :employees
 
     root to: "users#index"
   end
@@ -18,7 +19,7 @@ Rails.application.routes.draw do
       get :approve
     end
   end
-  
+
   devise_for :users, skip: [:registrations]
   root to: 'static#homepage'
 end
